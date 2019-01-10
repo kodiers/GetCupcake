260c8bdffed852b5492e57dac1b87a5e84cd4c77
Victor Yamchinov <kodiers@gmail.com>
2019-01-10 05:29:57 +0300

    added ui

---
+++ GetCupcake/OrderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OrderViewController.swift
+//  GetCupcake
+//
+//  Created by Viktor Yamchinov on 10/01/2019.
+//  Copyright © 2019 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var details: UILabel!
+    @IBOutlet weak var cost: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
